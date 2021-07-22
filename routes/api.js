0aaa10556640b5b4949e19a4d96d67f9e64d7790
Christi <mahomes4president@gmail.com>
2021-07-22 12:52:05 -0500

    adding api routes

---
+++ routes/api.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const { aggregate } = require("../models/workout.js");
+const Workout = require("../models/workout.js");
+const Transaction = require("../models/workout.js");
+
+
+
+
+
+router.post("/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/workout/range", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.put()
+
+
+
+
+
+router.get("/workout/range", (req, res) => {
+    
+    aggregate
+
+  
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/workout", (req, res) => {
+    aggregate
+}
+
+module.exports = router;
