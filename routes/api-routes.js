d2d552e388cc32bff2530adfde7e5a999815b6d2
Christi <mahomes4president@gmail.com>
2021-07-23 19:06:02 -0500

    adding to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,29 @@
 const db = require("../models");
 const router = require("express").Router();
 
+app.get("/api/workouts", function (req, res) {
+    db.Workout.find({}).then(function (dbWorkout) {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then(function (dbWorkout) {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+})
+
+
+app.get("/api/workouts/:id", function (req, res) {
+    var id = req.params.id;
+    db.Workout.findById(id, function (err, dbWorkout) {
+    }).catch(err => {
+        res.json(err);
+    });
+})
+
 module.exports = router;
