f194f6d025382118ac594063444a49397f1dcee8
Christi <mahomes4president@gmail.com>
2021-07-31 14:31:50 -0500

    fixing errors on server

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', { 
+  process.env.MONGODB_URI || 'mongodb://localhost/workout', { 
     useNewUrlParser: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    // useUnifiedTopology: true,
+    // useFindAndModify: false
 });
 
 
